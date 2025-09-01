09739f56dd110c549d9527ef461defde4beb82bf
Faylun <kauan.katysuky@gmail.com>
2025-09-01 02:03:22 -0300

    Adding new login screen

---
+++ views/login.view.php
@@ -0,0 +1,15 @@
+<div class="mt-6 grid grid-cols-2 gap-2">
+    <div class="border border-stone-700 rounded">
+        <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Login</h1>
+        <form action="/login" method="POST" class="px-4 py-2">
+            <div class="flex flex-col">
+                <label for="email" class="text-stone-400 mb-1">Email</label>
+                <input 
+                type="text" 
+                name="search" 
+                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1" 
+                placeholder="Pesquisar...">
+            </div>
+        </form>
+    </div>
+</div>
